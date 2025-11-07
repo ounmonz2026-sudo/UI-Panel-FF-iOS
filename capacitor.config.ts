d2f16850118b9b2c569801afb96767c01e42350f
ounmonz2026@gmail.com <ounmonz2026@gmail.com>
2025-11-07 14:36:34 +0000

    Initial commit with Capacitor setup for iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uipanel.app',
+  appName: 'UI Panel',
+  webDir: 'dist'
+};
+
+export default config;
